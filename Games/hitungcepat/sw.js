ccaf892fd94f498accd59369a9ecd3e298d0b8f2
Your Name <you@example.com>
2026-05-06 22:05:14 +0700

    first commit

---
+++ Games/hitungcepat/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "hitung-cepat-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./favicon.ico"
+  // Note: External audio files from mixkit.co cannot be cached reliably by SW due to CORS/opaque responses
+  // We rely on browser caching or the fallback beep for offline
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
